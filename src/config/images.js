d3232933175b647554c313169b0b2bbb88879009
pankaj-groovy <pankaj@groovyweb.firm.in>
2023-03-24 12:24:46 +0530

    Added: Static Images

--- src/config/images.js
+++ src/config/images.js
@@ -1,5 +1,9 @@
 const Images = {
   header_logo: "/assets/images/logo.png",
+  worksBg: "/assets/images/worksBg.png",
+  google: "/assets/images/google.png",
+  fb: "/assets/images/fb.png",
+  apple: "/assets/images/apple.png",
 };
 
 export default Images;
