e9d58b912c19a02608676cf9c275723b48e87624
pankaj-groovy <pankaj@groovyweb.firm.in>
2023-03-27 11:08:37 +0530

    Update: Handle Redux

--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
 import { ToastContainer } from "react-toastify";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { store, persistor } from "./redux/store/configureStore";
 import reportWebVitals from "./reportWebVitals";
 import theme from "./config/theme";
 import App from "./App";
@@ -9,12 +12,16 @@ import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <ThemeProvider theme={theme}>
-    <ToastContainer autoClose={3000} pauseOnFocusLoss={false} />
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </ThemeProvider>
+  <Provider store={store}>
+    <PersistGate loading={null} persistor={persistor}>
+      <ThemeProvider theme={theme}>
+        <ToastContainer autoClose={3000} pauseOnFocusLoss={false} />
+        <React.StrictMode>
+          <App />
+        </React.StrictMode>
+      </ThemeProvider>
+    </PersistGate>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
