b8754a010e246264e94a363fe3558aefaa22943b
pankaj-groovy <pankaj@groovyweb.firm.in>
2023-03-27 11:08:58 +0530

    Update: Added Link

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -7,6 +7,7 @@ import Images from "../../config/images";
 import useStyles from "./styles";
 
 function Header(props) {
+  const currentUrl = window.location.href;
   const classes = useStyles();
   const theme = useTheme();
   const isAddPadding = useMediaQuery(theme.breakpoints.down(1260));
@@ -43,15 +44,17 @@ function Header(props) {
           xs={8}
           className={classes.rightContainer}
         >
-          <Grid item className={classes.PR25}>
-            <NavLink to="" className={classes.linkStyle}>
-              <Typography className={classes.menuTitleStyle}>
-                Become a contractor
-              </Typography>
-            </NavLink>
-          </Grid>
+          {currentUrl?.includes("signup") ? null : (
+            <Grid item className={classes.PR25}>
+              <NavLink to="" className={classes.linkStyle}>
+                <Typography className={classes.menuTitleStyle}>
+                  Become a contractor
+                </Typography>
+              </NavLink>
+            </Grid>
+          )}
           <Grid item>
-            <NavLink to="" className={classes.linkStyle}>
+            <NavLink to="/login" className={classes.linkStyle}>
               <Button
                 variant="contained"
                 color="primary"
