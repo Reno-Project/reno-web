ea6627892a0c1c5166f3b62f83bf74faa7bc9cd9
pankaj-groovy <pankaj@groovyweb.firm.in>
2023-04-21 17:06:39 +0530

    Remove: Toast

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { ThemeProvider } from "@mui/material/styles";
-import { ToastContainer } from "react-toastify";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { store, persistor } from "./redux/store/configureStore";
@@ -15,7 +14,6 @@ root.render(
   <Provider store={store}>
     <PersistGate loading={null} persistor={persistor}>
       <ThemeProvider theme={theme}>
-        <ToastContainer autoClose={3000} pauseOnFocusLoss={false} />
         <React.StrictMode>
           <App />
         </React.StrictMode>
